8839fa8b539297b2551188a9c2c6542d51c2db57
ruben123-sudo <rubennieto2412@gmail.com>
2025-09-04 10:56:53 +0200

    Gurdando nuevos cambios

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './i18n';
 
 
@@ -17,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
